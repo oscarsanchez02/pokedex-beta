5b8017ec486bfb5b43138f209e2a913846595c8b
OscarSanti29 <osantiesteban69@hotmail.com>
2025-02-12 18:36:01 -0600

    new commit

--- src/pages/SinglePoke.jsx
+++ src/pages/SinglePoke.jsx
@@ -2,8 +2,29 @@ import { useState, useEffect } from "react";
 import { fetchPokemonById } from "../api";
 import { useParams } from "react-router-dom";
 
+const typeColors = {
+  normal: "#A8A878",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  dark: "#EE99AC",
+};
+
 export default function SinglePoke() {
-  const { pokemonID } = useParams();  // Get pokemonID from the URL
+  const { pokemonID } = useParams(); // Get pokemonID from the URL
   const [pokemon, setPokemon] = useState(null);
 
   useEffect(() => {
@@ -27,6 +48,32 @@ export default function SinglePoke() {
         alt={pokemon.name}
       />
       <p className="text-xl text-gray-700">ID: {pokemon.id}</p>
+      <p>{pokemon.weight}</p>
+      <div className="mb-4">
+        <h3 className="text-lg font-semibold">Types:</h3>
+        <div className="flex justify-center gap-2">
+          {" "}
+          {pokemon.types.map((type) => (
+            <span
+              key={type.type.name}
+              className="px-4 py-2 text-white rounded-full"
+              style={{ backgroundColor: typeColors[type.type.name] }}
+            >
+              {" "}
+              {type.type.name}{" "}
+            </span>
+          ))}{" "}
+        </div>
+      </div>
+      <div>
+        {pokemon.abilities.map((abilities, index) => {
+          return (
+            <div key={index}>
+              <div>{abilities.ability.name}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
